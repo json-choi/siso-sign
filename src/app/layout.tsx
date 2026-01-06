d5a751ca5dac41cb99134b4a37ccc86fe365b964
json-choi <cjs5241@gmail.com>
2026-01-06 14:34:48 +0900

    빌드 오류 수정(폰트 적용 테스트)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +11,13 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const calSans = localFont({
+  src: "./fonts/CalSans-Regular.woff2",
+  variable: "--font-cal-sans",
+  weight: "600",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${calSans.variable} antialiased`}
       >
         {children}
       </body>
